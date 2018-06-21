a0daf25b69bb8996a1706d6810cdaee39d94a1e0
LucaOll <luca.oldenburg@googlemail.com>
2018-06-21 16:12:05 +0200

    Update Matching

--- matching.js
+++ matching.js
@@ -1,12 +1,10 @@
-var bar = new ProgressBar.Path('#heart-path', {
-  easing: 'easeInOut',
-  duration: 5000,
-});
+function matchHeart(){
+  var bar = new ProgressBar.Path('#heart-path', {
+    easing: 'easeInOut',
+    duration: 1400
+  });
 
-bar.set(0);
-bar.animate(1.0);
-
-var loadFile = function(event,id) {
-  var output = document.getElementById(id);
-  output.src = URL.createObjectURL(event.target.files[0]);
+  bar.set(0);
+  var random = Math.random()*(1.01-0.0)+0.0;
+  bar.animate(random);
 };
