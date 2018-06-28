714a279900d9acc67130aedbdbd591497255a99e
LucaOll <luca.oldenburg@googlemail.com>
2018-06-28 13:33:10 +0200

    Update des Kebab Match

--- matching.js
+++ matching.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-var bar = new ProgressBar.Path('#heart-path', {
-  easing: 'easeInOut',
-  duration: 5000,
-});
-
-bar.set(0);
-bar.animate(1.0);
-
-var loadFile = function(event,id) {
-  var output = document.getElementById(id);
-  output.src = URL.createObjectURL(event.target.files[0]);
-};
-=======
 function matchHeart(){
   var bar = new ProgressBar.Path('#heart-path', {
     easing: 'easeInOut',
@@ -22,4 +8,8 @@ function matchHeart(){
   var random = Math.random()*(1.01-0.0)+0.0;
   bar.animate(random);
 };
->>>>>>> e404fb22ace9e9ec4e4741006a5d2bb9fce8fc1a
+
+var loadFile = function(event,id) {
+  var output = document.getElementById(id);
+  output.src = URL.createObjectURL(event.target.files[0]);
+};
